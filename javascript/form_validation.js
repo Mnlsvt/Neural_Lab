8c6ddc8ca2131e1eaf55a27466e387ae7d20e562
IsabellaPap <p19papa11@ionio.gr>
2022-05-06 18:33:37 +0300

    Create form_validation.js

---
+++ javascript/form_validation.js
@@ -0,0 +1,12 @@
+const fullname = document.getElementById("fullname")
+const fathername = document.getElementById("fathername")
+const age = document.getElementById("age")
+const phone = document.getElementById("telno")
+const email = document.getElementById("email")
+const taxno = document.getElementById("taxno")
+const securityno = document.getElementById("securityno")
+const form = document.getElementById("form")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
