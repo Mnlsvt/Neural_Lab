a21293405c58c0f6ce7022cb6ae7c5306dd9ad9b
IsabellaPap <p19papa11@ionio.gr>
2022-05-10 18:51:43 +0300

    Update form_validation.js and subscription.html

--- javascript/form_validation.js
+++ javascript/form_validation.js
@@ -1,16 +1,25 @@
 function validation()
-{
-    // Full name one space between, no special chars
-    var fullname = document.getElementById("fullname")
-    //var fullnameRGEX =
-
-    var fathername = document.getElementById("fathername")
-    var age = document.getElementById("age")
-    var phone = document.getElementById("telno")
-    var email = document.getElementById("email")
-    var taxno = document.getElementById("taxno")
-    var securityno = document.getElementById("securityno")
+{   
+    
     var form = document.getElementById("form")
 
+    // Full name one space between, no special chars
+    var fullname = document.getElementById("fullname").value;
+    var fullnameRGEX = /^[a-zA-Z]+\s[a-zA-Z]+$/;
+    var fullnameResult = fullnameRGEX.test(fullname);
+
+    if(fullnameResult == false)
+    {
+    alert("Please input your first name and last name separated by a single space and don't use special characters");
+    }
+  
+    // var fathername = document.getElementById("fathername")
+    // var age = document.getElementById("age")
+    // var phone = document.getElementById("telno")
+    // var email = document.getElementById("emailadd")
+    // var taxno = document.getElementById("taxno")
+    // var securityno = document.getElementById("securityno")
+
     
+
 }
